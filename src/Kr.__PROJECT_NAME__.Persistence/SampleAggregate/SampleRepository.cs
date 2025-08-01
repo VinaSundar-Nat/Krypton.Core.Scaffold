1e9632dcb7c2f156731d0847c7f17a76d369b9b7
VinaSundar-Nat <vina.sundar@gmail.com>
2025-08-02 09:04:56 +1000

    common referance upgrade and action clean up

--- src/Kr.__PROJECT_NAME__.Persistence/SampleAggregate/SampleRepository.cs
+++ src/Kr.__PROJECT_NAME__.Persistence/SampleAggregate/SampleRepository.cs
@@ -1,8 +1,27 @@
 using System;
+using Kr.__PROJECT_NAME__.Persistence.SampleAggregate.Entity;
+using Kr.Common.Infrastructure.Datastore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Kr.__PROJECT_NAME__.Persistence.SampleAggregate;
 
-public class SampleRepository
+public interface ISampleRepository
 {
+    Task<Sample> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<IList<Sample>> GetAllAsync(CancellationToken cancellationToken = default);
+}
 
+public class SampleRepository(ILogger<SampleRepository> logger, DbContext context) 
+    : BaseRepository<Sample>(logger, context), ISampleRepository
+{
+    public Task<IList<Sample>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Sample> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
 }
